913c265afa8b9ef348a24edd095f3590d6f685a0
David Chen <chentiki808@gmail.com>
2016-03-23 21:49:15 -0700

    Discussion Board, association

---
+++ 6_Disscussion_Board/server/config/routes.js
@@ -0,0 +1,27 @@
+// ROUTES.JS
+
+// require topic controller
+var topic = require('./../controllers/topics.js');
+
+// require comment controller
+var comment = require('./../controllers/comments.js');
+
+// require answer controller
+var answer = require('./../controllers/answers.js');
+
+
+
+module.exports = function(app){
+
+	// GETS ===========================================
+	
+
+
+
+	// POSTS ===========================================
+	
+
+
+
+
+};
---
+++ 6_Disscussion_Board/server/controllers/topics.js
@@ -0,0 +1,18 @@
+// TOPIC CONTROLLER
+var mongoose = require('mongoose');
+var Topic = mongoose.model('Topic');
+
+module.exports = (function(){
+
+	return {
+
+		
+
+
+	} // end return
+
+
+
+
+
+})(); // end module.exports
